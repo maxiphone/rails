f9ea140dbfe351b7b6ef883e5e95f10f519bf0d7
maxiphone4ios7 <musta@Mac-mini-de-musta.local>
2016-02-22 14:20:24 +0100

    app inicial

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+	validates :nombre, presence: true, length: { minimum: 3 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+end
